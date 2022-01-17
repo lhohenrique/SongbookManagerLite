19ae28468e8a2f7b2980b2fff257587fd878a9d3
SAMSUNGPROJECT\lho <lho@cin.ufpe.br>
2022-01-17 16:13:31 -0300

    Create music test command

--- SongbookManagerLite/SongbookManagerLite/SongbookManagerLite/ViewModels/MusicPageViewModel.cs
+++ SongbookManagerLite/SongbookManagerLite/SongbookManagerLite/ViewModels/MusicPageViewModel.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace SongbookManagerLite.ViewModels
 {
-    class MusicPageViewModel
+    public class MusicPageViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                name = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        public ICommand NewMusic
+        {
+            get => new Command(() =>
+            {
+                Application.Current.MainPage.DisplayAlert("Alerta", "Nova música", "OK");
+            });
+        }
     }
 }
