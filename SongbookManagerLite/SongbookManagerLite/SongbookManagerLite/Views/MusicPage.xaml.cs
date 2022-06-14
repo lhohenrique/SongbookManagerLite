cf4ac876914a3552b50d79e8c546f2e6e2a9003b
SAMSUNGPROJECT\lho <lho@cin.ufpe.br>
2022-06-14 15:32:11 -0300

    Change search bar to use text changed event

--- SongbookManagerLite/SongbookManagerLite/SongbookManagerLite/Views/MusicPage.xaml.cs
+++ SongbookManagerLite/SongbookManagerLite/SongbookManagerLite/Views/MusicPage.xaml.cs
@@ -25,5 +25,11 @@ namespace SongbookManagerLite.Views
             var viewModel = (MusicPageViewModel)BindingContext;
             viewModel.UpdateMusicListCommand.Execute(null);
         }
+
+        private void MusicSearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var viewModel = (MusicPageViewModel)BindingContext;
+            viewModel.SearchCommand.Execute(null);
+        }
     }
 }
