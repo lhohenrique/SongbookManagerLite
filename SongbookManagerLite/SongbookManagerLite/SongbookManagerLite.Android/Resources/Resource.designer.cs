583920023d7e9463d7c3b352ced650759472c6cb
SAMSUNGPROJECT\lho <lho@cin.ufpe.br>
2022-07-04 00:13:30 -0300

    Apply style to Musics page

--- SongbookManagerLite/SongbookManagerLite/SongbookManagerLite.Android/Resources/Resource.designer.cs
+++ SongbookManagerLite/SongbookManagerLite/SongbookManagerLite.Android/Resources/Resource.designer.cs
@@ -15233,55 +15233,67 @@ namespace SongbookManagerLite.Droid
 			public const int mtrl_tabs_default_indicator = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int navigation_empty_icon = 2131165314;
+			public const int musics = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int notification_action_background = 2131165315;
+			public const int navigation_empty_icon = 2131165315;
 			
 			// aapt resource value: 0x7F070084
-			public const int notification_bg = 2131165316;
+			public const int notification_action_background = 2131165316;
 			
 			// aapt resource value: 0x7F070085
-			public const int notification_bg_low = 2131165317;
+			public const int notification_bg = 2131165317;
 			
 			// aapt resource value: 0x7F070086
-			public const int notification_bg_low_normal = 2131165318;
+			public const int notification_bg_low = 2131165318;
 			
 			// aapt resource value: 0x7F070087
-			public const int notification_bg_low_pressed = 2131165319;
+			public const int notification_bg_low_normal = 2131165319;
 			
 			// aapt resource value: 0x7F070088
-			public const int notification_bg_normal = 2131165320;
+			public const int notification_bg_low_pressed = 2131165320;
 			
 			// aapt resource value: 0x7F070089
-			public const int notification_bg_normal_pressed = 2131165321;
+			public const int notification_bg_normal = 2131165321;
 			
 			// aapt resource value: 0x7F07008A
-			public const int notification_icon_background = 2131165322;
+			public const int notification_bg_normal_pressed = 2131165322;
 			
 			// aapt resource value: 0x7F07008B
-			public const int notification_template_icon_bg = 2131165323;
+			public const int notification_icon_background = 2131165323;
 			
 			// aapt resource value: 0x7F07008C
-			public const int notification_template_icon_low_bg = 2131165324;
+			public const int notification_template_icon_bg = 2131165324;
 			
 			// aapt resource value: 0x7F07008D
-			public const int notification_tile_bg = 2131165325;
+			public const int notification_template_icon_low_bg = 2131165325;
 			
 			// aapt resource value: 0x7F07008E
-			public const int notify_panel_notification_icon_bg = 2131165326;
+			public const int notification_tile_bg = 2131165326;
 			
 			// aapt resource value: 0x7F07008F
-			public const int splash_screen = 2131165327;
+			public const int notify_panel_notification_icon_bg = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int test_custom_background = 2131165328;
+			public const int profile = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int tooltip_frame_dark = 2131165329;
+			public const int repertoire = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int tooltip_frame_light = 2131165330;
+			public const int share = 2131165330;
+			
+			// aapt resource value: 0x7F070093
+			public const int splash_screen = 2131165331;
+			
+			// aapt resource value: 0x7F070094
+			public const int test_custom_background = 2131165332;
+			
+			// aapt resource value: 0x7F070095
+			public const int tooltip_frame_dark = 2131165333;
+			
+			// aapt resource value: 0x7F070096
+			public const int tooltip_frame_light = 2131165334;
 			
 			static Drawable()
 			{
@@ -17799,6 +17811,9 @@ namespace SongbookManagerLite.Droid
 			// aapt resource value: 0x7F0F00F0
 			public const int MaterialAlertDialog_MaterialComponents_Title_Text_CenterStacked = 2131689712;
 			
+			// aapt resource value: 0x7F0F02BF
+			public const int myCustomMenuTextAppearance = 2131690175;
+			
 			// aapt resource value: 0x7F0F00F1
 			public const int Platform_AppCompat = 2131689713;
 			
@@ -17892,11 +17907,11 @@ namespace SongbookManagerLite.Droid
 			// aapt resource value: 0x7F0F010F
 			public const int RtlUnderlay_Widget_AppCompat_ActionButton_Overflow = 2131689743;
 			
-			// aapt resource value: 0x7F0F02BF
-			public const int scrollViewScrollBars = 2131690175;
-			
 			// aapt resource value: 0x7F0F02C0
-			public const int scrollViewTheme = 2131690176;
+			public const int scrollViewScrollBars = 2131690176;
+			
+			// aapt resource value: 0x7F0F02C1
+			public const int scrollViewTheme = 2131690177;
 			
 			// aapt resource value: 0x7F0F0116
 			public const int ShapeAppearanceOverlay = 2131689750;
