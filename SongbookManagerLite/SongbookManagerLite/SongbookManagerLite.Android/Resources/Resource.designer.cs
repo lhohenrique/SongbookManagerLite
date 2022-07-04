50109c26d2fa9ab04a28b5594b0d20c8ac1ec702
SAMSUNGPROJECT\lho <lho@cin.ufpe.br>
2022-07-04 15:38:40 -0300

    Apply style to Add Musics page

--- SongbookManagerLite/SongbookManagerLite/SongbookManagerLite.Android/Resources/Resource.designer.cs
+++ SongbookManagerLite/SongbookManagerLite/SongbookManagerLite.Android/Resources/Resource.designer.cs
@@ -15281,19 +15281,22 @@ namespace SongbookManagerLite.Droid
 			public const int repertoire = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int share = 2131165330;
+			public const int save = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int splash_screen = 2131165331;
+			public const int share = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int test_custom_background = 2131165332;
+			public const int splash_screen = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int tooltip_frame_dark = 2131165333;
+			public const int test_custom_background = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int tooltip_frame_light = 2131165334;
+			public const int tooltip_frame_dark = 2131165334;
+			
+			// aapt resource value: 0x7F070097
+			public const int tooltip_frame_light = 2131165335;
 			
 			static Drawable()
 			{
